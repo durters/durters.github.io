11d5cbcb73749f0f58b2f84ec096ffb7e2a48073
gbokman <bokman@student.chalmers.se>
2019-08-02 16:22:14 +0200

    added stupid render files to circumvent stupid python argument passing

---
+++ render.bat
@@ -0,0 +1,2 @@
+:: Short script to run lilypond-book, change to appropriate path if necessary
+"C:\Program Files (x86)\LilyPond\usr\bin\python" "C:\Program Files (x86)\LilyPond\usr\bin\lilypond-book.py" %1
